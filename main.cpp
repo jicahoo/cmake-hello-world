f1d4bb45cdfe0a6221509546943bb26c305ea7e7
Jichao Zhang <jicahoo@163.com>
2020-02-27 03:20:57 -0800

    refine

--- main.cpp
+++ main.cpp
@@ -11,7 +11,36 @@ optional<int> get_even_number() {
     return i % 2 == 0 ? i : optional<int>();
 }
 
-int main() {
+void changeBool(bool &b){
+    b = true;
+}
+
+class Apple
+{
+public:
+    Apple(int pricePara):price(pricePara){
+
+    }
+    void grow()
+    {
+        std::cout << "I am growing!\n";
+    }
+    void sell() {
+        std::cout << "Price is " << this->price << std::endl;
+    }
+private:
+    int price;
+};
+
+void handleApple(Apple & apple) {
+    apple.sell();
+}
+
+void handleApplePtr(Apple* & apple) {
+    apple->sell();
+}
+
+void example_optional() {
     std::srand(static_cast<unsigned int>(std::time(0)));
     cout << "Hello, World! A even number: " << std::endl;
     optional<int> j = get_even_number();
@@ -20,5 +49,23 @@ int main() {
     } else {
         cout << "Empty optional value." << std::endl;
     }
+    bool a = false;
+    changeBool(a);
+    cout << a << std::endl;
+}
+
+void example_class() {
+    Apple apple(10);
+    apple.grow();
+    Apple* applePtr = &apple;
+    applePtr->grow();
+    applePtr->sell();
+    handleApple(apple);
+    handleApplePtr(applePtr);
+}
+
+int main() {
+    example_class();
+
     return 0;
 }
