eefcebe2316834898e88cddf0015053784a46703
Jichao Zhang <jicahoo@163.com>
2020-02-29 17:52:42 -0800

    example of share lib

---
+++ 02-shared-library/lib-src/hello.cpp
@@ -0,0 +1,10 @@
+//
+// Created by jichao on 2/29/20.
+//
+
+#include "hello.h"
+#include <iostream>
+
+void say_hello() {
+    std::cout << "Hello!" << std::endl;
+}
---
+++ 02-shared-library/lib-src/hello.h
@@ -0,0 +1,10 @@
+//
+// Created by jichao on 2/29/20.
+//
+
+#ifndef CMAKE_HELLO_WORLD_HELLO_H
+#define CMAKE_HELLO_WORLD_HELLO_H
+
+void say_hello();
+
+#endif //CMAKE_HELLO_WORLD_HELLO_H
